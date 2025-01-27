7ffe919bcbdc4258e8e96919c398301321773538
noeru_desu <3203911711@qq.com>
2025-01-27 21:57:30 +0800

    添加失败提醒

--- aruCraftR-v1.0.0/arucraftr/websocket/__init__.py
+++ aruCraftR-v1.0.0/arucraftr/websocket/__init__.py
@@ -8,6 +8,5 @@ from arucraftr.websocket.types import WebSocketMessage
 async def send_msg(message: WebSocketMessage):
     if shared.ws_connection is not None and shared.ws_connection.state == State.OPEN:
         await shared.ws_connection.send(message.json, True)
-
-
-
+    else:
+        shared.plg_server_inst.logger.warning('ws连接尚未建立, 无法发送消息')
